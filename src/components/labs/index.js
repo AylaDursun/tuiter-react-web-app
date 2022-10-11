46b6d3fbe2d2f17418bce7245572ef4e2d7f827b
AylaDursun <dursun.a@northeastern.edu>
2022-10-10 23:08:39 -0400

    A6 lab fall 2022

---
+++ src/components/labs/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import TodoList from "./todo/TodoList";
+import TodoItem from "./todo/TodoItem";
+
+
+const Labs = () => {
+    return(
+        <div>
+            <h1>Labs</h1>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+            <TodoItem/>
+            <TodoList/>
+        </div>
+    )
+};
+
+export default Labs;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
 );
+// core library
+// library for rendering into DOM element
+// application entry point, can be called anything, but App is common
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// use library to render
+// App
+// into element whose ID is root, the one declared earlier in index.html
+
+
