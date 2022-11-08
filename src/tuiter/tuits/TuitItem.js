0579e4f6d464dd182844d54826287f5a997076f9
AylaDursun <dursun.a@northeastern.edu>
2022-11-08 13:56:16 -0500

    A8 start fall 2022

--- src/tuiter/tuits/TuitItem.js
+++ src/tuiter/tuits/TuitItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import Json from "../../json";
 import TuitStats from "./TuitStats";
 import {useDispatch} from "react-redux";
-import {deleteTuit} from "../reducers/tuits-reducer";
+import {deleteTuitThunk} from "../../services/tuits-thunks";
 const TuitItem = (
     {
         tuit = {
@@ -16,7 +16,7 @@ const TuitItem = (
 ) => {
     const dispatch = useDispatch();
     const deleteTuitHandler = (id) => {
-        dispatch(deleteTuit(id));
+        dispatch(deleteTuitThunk(id));
     }
     return(
         <li className="list-group-item">
